761648b33695e97d02f94d1eaac25e5221f4fc12
Yuri Frota <yurisaboiaf@gmail.com>
2022-04-24 17:16:17 -0300

    Initial Commit

---
+++ MazeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MazeApp
+//
+//  Created by Yuri on 24/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
